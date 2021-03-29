9a6401c823e036c5c0ad7e0b08a1571b9e5c8be3
raghav-kukreti <raghav.kukreti@gmail.com>
2021-03-29 06:12:36 +0530

    Init bootstrap, hardhat + svelte

---
+++ contracts/Token.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+contract Token {
+    string public name = "MUZEK";
+    string public symbol = "MZK";
+
+    uint256 public totalSupply = 1000000;
+}
